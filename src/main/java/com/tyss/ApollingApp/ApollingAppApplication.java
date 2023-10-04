213741a8c8324a850228457305f99e3d61a80aa0
govardhantsm <govardhanpvg@gmail.com>
2023-10-04 12:20:47 +0530

    project pushed to master

---
+++ src/main/java/com/tyss/ApollingApp/ApollingAppApplication.java
@@ -0,0 +1,13 @@
+package com.tyss.ApollingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApollingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApollingAppApplication.class, args);
+	}
+
+}
