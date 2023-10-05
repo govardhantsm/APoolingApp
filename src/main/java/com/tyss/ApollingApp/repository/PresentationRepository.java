bba3f040b87cc160a7b910d40385916f01c2fa6c
govardhantsm <govardhanpvg@gmail.com>
2023-10-05 10:07:56 +0530

    userdao

--- src/main/java/com/tyss/ApollingApp/repository/PresentationRepository.java
+++ src/main/java/com/tyss/ApollingApp/repository/PresentationRepository.java
@@ -3,9 +3,10 @@ package com.tyss.ApollingApp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tyss.ApollingApp.entity.Presentation;
+import com.tyss.ApollingApp.entity.User;
 
 public interface PresentationRepository extends JpaRepository<Presentation, Integer> {
 
 	
-	
+   
 }
--- src/main/java/com/tyss/ApollingApp/repository/UserRepository.java
+++ src/main/java/com/tyss/ApollingApp/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.tyss.ApollingApp.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tyss.ApollingApp.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer>{
-                
+public interface UserRepository extends JpaRepository<User, Integer> {
+	@Query(value = "SELECT u FROM User u WHERE u.user_email=?1 AND u.user_password=?2")
+	User findByUser_emailAndUser_password(String Email, String Password);
 }
